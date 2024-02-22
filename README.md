6cbec8c88ffa87c3936dc5a810fc7e1c9911e708
Daniella Tuesta <daniella.tuesta2@gmail.com>
2024-02-22 10:50:44 -0500

    final

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Spreadsheet Analysis
-A little assignment to practice finding data, munging it, and analyzing it in a spreadsheet program.
+## Data set details
+Released by Netflix, this dataset illustrates, for the first time in the streaming giant's history, what people watched on the platform over a six month period (Jan 2023 - June 2023). [https://about.netflix.com/en/news/what-we-watched-a-netflix-engagement-report](Dataset Link: Netflix Engagement Report).
 
-Replace the contents of this file with a report, as described in the [instructions](./instructions.md).
+This dataset was originally in a .XSLV file, so I downloaded the file and then exported it as a CSV. Here are the first 20 rows of my clean_data.csv:
+
+The only problem I found in the original dataset was the missing data for a TV show's or film's release data. I solved this issue by simply replacing the empty slots with "NaN" indicators Additionally, I went ahead and divided the Hours Viewed column by 1,000,000 to get a better understanding of how big or small of an audience each Netflix TV series or film pulled. I also added a new column that illustrated if a title was a TV Show, however I could identify a group of Netflix's TV catalog because I used keywords like "Season" and "Temporada" which not all TV Shows had in their titles. There is without a doubt many more TV shows. Lastly, I changed the Release Date column to 'Premiere Year' to make comparing titles by date easier. To accomplish all of these tasks, I divided lines by those I could split with "," and those I could not due to a "," being in the title. Throughout this process, I had several errors where the added values would appear in between a title string or in between the hours viewed column. I largely fixed this by rewriting each line and going step by step to ensure that everything was being executed the way it was meant to.
+
+Links to your data files (the original raw data, the munged data, and the spreadsheet file including the formulas and charts). These can be links to files in your own repository or links to the files stored in a cloud storage service if your files are too large to be included in your own repository.
+
+
+
+Analysis:
--- munge.py
+++ munge.py
@@ -1,1 +1,134 @@
-# place your code to clean up the data file below.
+import os
+# Define file paths for the input raw data and the output clean data
+filepathn = os.path.join('data', 'netflix_data.csv')
+file_obj = open(filepathn,'r')
+
+filepathc = os.path.join('data', 'clean_data.csv')
+new_file = open(filepathc,'w')
+
+
+def edit(line):
+    new_list = []
+    new_line = ""
+
+    # working with titles that have "," 
+    if line[0] == '"':
+
+        quotations_count = 0 
+        for i in line:
+            if i == '"':
+                quotations_count += 1
+        
+        # points with no commas in title will have 4 commas throughout lime
+        # split successful and there are no issues with ',' in the title
+        if quotations_count == 4:
+
+            title_str = '' 
+            new_line += '"'
+            index = 2   # counter variable to record where the title ends
+
+            for i in line[1::]:
+                index += 1
+                if i != '"':
+                    title_str += i
+                    new_line += i 
+                else: 
+                    title_str += i
+                    new_list += [title_str]
+                    new_line += i + ","
+                    break
+
+        # titles with '""' and no commas
+        else:
+            title_str = '' 
+            new_line += '"'
+            index = 1   # counter variable to record where the title ends
+            for i in line[1::]:
+                index += 1
+                if i != ",":
+                    title_str += i
+                else:
+                    break
+            
+            new_line += title_str + ','
+
+
+    else:
+        title_str = ''
+        index = 0 
+        for char in line: 
+            index += 1
+            if char != ',':
+                title_str  += char
+                new_line += char
+            else:   # title is complete, now add to new_list 
+                new_list += [title_str]
+                new_line += ","
+                break
+    
+    # adding column to determine if title is a TV show (not all TV shows can be identified)
+    
+    for keyword in ["Season","Temporada"]:
+        if title_str.find(keyword) != -1:
+            new_line += 'Yes,'
+            series = True
+            break
+        else:
+            series = False
+
+    
+    if series == False:
+        new_line += 'NaN,'
+
+        
+    split_str = line[index:].strip().split(",")
+
+    for i in [0,1]:
+        if i == 0:
+            new_line += split_str[i]+','
+
+        elif i == 1:
+            date = split_str[i]
+            # handling missing data 
+            if date == "":
+                new_line += "NaN,"
+            else:
+                year = date[0:4]
+                new_line += year+','
+    
+
+    # creating new viewership measurement
+    value = ""
+    for i in range(2,len(split_str)):
+        for char in split_str[i]:
+            if char.isnumeric():
+                value += char
+
+
+    value_div_by1M = int(value)/1000000
+    new_list += [str(float(value_div_by1M))]
+    new_line += str(float(value_div_by1M))
+
+    return new_line
+ 
+
+
+
+# skip irrelevant lines 
+for i in range(6):
+    line = file_obj.readline()
+
+# write columns titles and replace "release date" for "release year" and "release month"
+columns = line[1::].strip()
+columns= columns.split(',')
+
+
+new_file.write("Title,TV Show,Available Globally?,Premiere Year,Hours Viewed (in 1M units)"+'\n')
+
+
+# iterating through each line containing streaming data for each title
+for line in file_obj:
+        # discarding intial ',' char -- irrelevant
+    line = line[1::]
+
+    new_file.write(str(edit(line))+'\n')
